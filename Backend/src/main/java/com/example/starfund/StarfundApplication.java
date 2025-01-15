863d4b6e171a64854d63c582bb7a96fd4810a99c
ManuDev <vargas_manuel08@hotmail.com>
2025-01-14 22:49:07 -0500

    feat: Initialized backend structure

---
+++ Backend/src/main/java/com/example/starfund/StarfundApplication.java
@@ -0,0 +1,13 @@
+package com.example.starfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarfundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarfundApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/starfund/StarfundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarfundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
